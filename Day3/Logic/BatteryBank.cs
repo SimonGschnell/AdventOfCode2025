8013f101125835eef78997ba3fbc402807fee3c2
Simon Gschnell <simongschnell@gmail.com>
2025-12-03 21:28:22 +0100

    adds solution for day3 part 2

--- Day3/Logic/BatteryBank.cs
+++ Day3/Logic/BatteryBank.cs
@@ -9,14 +9,67 @@ public class BatteryBank
         Batteries = batteries;
     }
 
-    public int GetJoltage()
+    public long GetJoltage(int N=2)
     {
+        //818181911112111
         var greatesBatteryCombination = 0;
-        foreach (var firstBattery in Batteries)
+        
+        
+        var result = new List<int>();
+
+        var numbersToCollectLeft = 0;
+        var rangeWhereToFindNextNumber = Batteries.Slice(0, Batteries.Count - (N - 1));
+        var lastIndex = 0;
+        var nextIndex = 0;
+        while(true)
         {
-            greatesBatteryCombination = GetGreatestBatteryCombination(firstBattery);
+            var (greatestNumber, greatestIndex) = GreatestNumber(rangeWhereToFindNextNumber);
+            nextIndex = nextIndex + greatestIndex + 1;
+            result.Add(greatestNumber);
+            numbersToCollectLeft = N - result.Count;
+            var lengthToEnd = Batteries.Count-(nextIndex);
+            lastIndex = nextIndex-1;
+            if (result.Count == N || numbersToCollectLeft == Batteries.Slice(nextIndex,lengthToEnd).Count)
+            {
+                break;
+            }
+
+            
+            rangeWhereToFindNextNumber =  Batteries.Slice(nextIndex, (Batteries.Count- (nextIndex)) - (N - 1 - result.Count));
+        } 
+
+        if (result.Count() != N)
+        {
+            for (int i = lastIndex+1; i < Batteries.Count; i++)
+            {
+                result.Add(Batteries[i].Joltage);
+            }
         }
-        return greatesBatteryCombination;
+
+        var res = "";
+        foreach (var i in result)
+        {
+            res += i.ToString();
+        }
+
+        return long.Parse(res);
+
+    }
+
+    private static (int greatestNumber, int greatestIndex) GreatestNumber(List<Battery> subarray)
+    {
+        var greatestNumber = 0;
+        var greatestIndex = 0;
+        for(var index =0; index < subarray.Count; index++)
+        {
+            if (subarray[index].Joltage > greatestNumber)
+            {
+                greatestNumber = subarray[index].Joltage;
+                greatestIndex = index;
+            }
+        }
+
+        return (greatestNumber, greatestIndex);
     }
 
     private int GetGreatestBatteryCombination(Battery firstBattery)
